92f2886f2e541cb651f60a4210262a685d04a1fa
Adrian Ariton <adrianariton@Adrian-Minune.local>
2023-09-27 20:57:49 +0300

    again

--- ChatTutor/chattutor_appengine/definitions.py
+++ ChatTutor/chattutor_appengine/definitions.py
@@ -4,10 +4,31 @@ from pydantic import BaseModel
 DocKey = Any
 
 class Doc(BaseModel):
+    """Doc class that characterizes a paper. It is used
+    as an identificator for texts inside the document.
+    
+    Attributes:
+        docname (str): name of the document
+        ciration (str): citation
+        dockey (DocKey - Any): dockey
+
+    Args:
+        BaseModel
+    """
     docname: str
     citation: str
     dockey: DocKey
 
 class Text(BaseModel):
+    """Texts from a document that can be added to databases
+    or used for embeddings
+    
+    Attributes:
+        text (str): text 
+        doc (Doc): document
+
+    Args:
+        BaseModel
+    """
     text: str
     doc: Doc
--- ChatTutor/chattutor_appengine/extensions.py
+++ ChatTutor/chattutor_appengine/extensions.py
@@ -1,3 +1,7 @@
+"""
+    Defines which database is used. One can choose between chroma and deeplake
+"""
+
 from database import VectorDatabase
 
-db = VectorDatabase("./db", 'deeplake_tensordb')
+db = VectorDatabase("./db", 'chroma')
