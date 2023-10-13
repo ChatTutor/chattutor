d1884d0ac8c37d8cfd381ab715009d508fc8cc22
Aatmik Mallya <aatmik54@gmail.com>
2023-10-13 17:09:53 -0400

    change default model on backend, and frontend

--- ChatTutor/main.py
+++ ChatTutor/main.py
@@ -169,8 +169,7 @@ def ask():
     from_doc = data.get("from_doc")
     selected_model = data.get('selectedModel')
     if selected_model == None:
-        # selected_model = 'gpt-3.5-turbo-16k'
-        selected_model = 'gpt-4'
+        selected_model = 'gpt-3.5-turbo-16k'
     print('SELECTED MODEL:', selected_model)
     print(collection_name)
     # Logging whether the request is specific to a document or can be from any document
