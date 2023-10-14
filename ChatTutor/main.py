91a0d35b330bff3daa05250df4899b1072559350
Aatmik Mallya <aatmik54@gmail.com>
2023-10-14 19:03:59 -0400

    actually fix cloud issue

--- ChatTutor/main.py
+++ ChatTutor/main.py
@@ -43,7 +43,7 @@ from werkzeug.datastructures import FileStorage
 # from vectordatabase import VectorDatabase
 
 interpreter.auto_run = True
-if "CHATUTOR_GCP" in os.environ or "_CHATUTOR_GCP" in os.environ:
+if "CHATTUTOR_GCP" in os.environ or "_CHATUTOR_GCP" in os.environ:
     openai.api_key = os.environ["OPENAI_API_KEY"]
 else:
     import yaml
