47dfe3bb99d230c2d8b41ee65a989f3740997bbf
Aatmik Mallya <aatmik54@gmail.com>
2023-10-18 19:55:07 -0400

    add components

---
+++ ChatTutor/frontend/src/app/input-box/input-box.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-input-box',
+  templateUrl: './input-box.component.html',
+  styleUrls: ['./input-box.component.css']
+})
+export class InputBoxComponent {
+
+}
