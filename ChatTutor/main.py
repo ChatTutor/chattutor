81c6c60be1304f2ebb0b5e77881129295d288e1f
bernardo <bernardo.casabone@meetoptics.com>
2023-10-22 10:36:40 +0300

    including cqn welcoming message

--- ChatTutor/main.py
+++ ChatTutor/main.py
@@ -1,7 +1,8 @@
 import flask
 from flask import Flask, request, redirect, send_from_directory, url_for
 from flask import stream_with_context, Response, abort, jsonify
-from flask_cors import CORS  # Importing CORS to handle Cross-Origin Resource Sharing
+from flask_cors import CORS
+from nice_functions import pprint, time_it 
 from core.extensions import (
     db,
     user_db,
@@ -14,7 +15,7 @@ from core.tutor import cqn_system_message, default_system_message
 import json
 import time
 import os
-print('lllllllllllllll',os.environ)
+# print('lllllllllllllll',os.environ)
 from core.reader import URLReader
 from core.definitions import Text
 from core.definitions import Doc
@@ -42,17 +43,9 @@ from werkzeug.datastructures import FileStorage
 # from vectordatabase import VectorDatabase
 
 interpreter.auto_run = True
-if "CHATTUTOR_GCP" in os.environ or "_CHATUTOR_GCP" in os.environ:
-    openai.api_key = os.environ["OPENAI_API_KEY"]
-else:
-    import yaml
+from core.openai_tools import load_api_keys
+load_api_keys()
 
-    with open(".env.yaml") as f:
-        yamlenv = yaml.safe_load(f)
-    keys = yamlenv["env_variables"]
-    os.environ["OPENAI_API_KEY"] = keys["OPENAI_API_KEY"]
-    openai.api_key = keys["OPENAI_API_KEY"]
-    os.environ["ACTIVELOOP_TOKEN"] = keys["ACTIVELOOP_TOKEN"]
 
 app = Flask(__name__)
 CORS(app)  # Enabling CORS for the Flask app to allow requests from different origins
@@ -139,7 +132,51 @@ def cqn():
     response with information from its database of papers and information.
     Redirects the root URL to the index.html in the static folder
     """
-    return redirect(url_for("static", filename="cqn.html"))
+
+    load_api_keys()
+    tutor = Tutor(db)
+    db.load_datasource("test_embedding_basic")
+    print("getting number of documents...")
+    docs = db.datasource.get(include=[])
+    total_papers = len(docs["ids"]) 
+    pprint("total_papers", total_papers)
+    print("generating welcoming message...")
+
+    welcoming_message = f"""
+                <p>Welcome to the Center for Quantum Networks (CQN) website! I am your Interactive Research Assistant, here to assist you in exploring the wealth of knowledge within the CQN research database. With access to a vast collection of <b>{total_papers}</b> research papers, I am equipped to provide insightful and accurate responses to your queries.</p>
+                <p>Whether you are looking for papers by a specific author, papers from a particular date or journal, or papers related to a specific topic or subject, I've got you covered. I can also help you find similar papers to ones you already know or even provide paper summaries.</p>
+                <p>Here are some examples of questions you can ask:</p>
+                <ul>
+                    <li>"Can you find papers authored by John Doe?"</li>
+                    <li>"What papers were published in the year 2020?"</li>
+                    <li>"Can you recommend papers related to quantum entanglement?"</li>
+                    <li>"Are there any similar papers to the one titled 'Quantum Computing Advancements'?"</li>
+                    <li>"Can you summarize the paper titled 'Quantum Networking Protocols' for me?"</li>
+                </ul>
+                <p>Feel free to explore the CQN research database and ask any questions you may have. I'm here to assist you on your research journey!</p>    
+    """
+    
+    # welcoming_message = "" # disable to generate a new one using simple_gpt
+    if welcoming_message == "":
+        welcoming_message =  time_it(tutor.simple_gpt)(f"""
+        You are embedded into the Center for Quantum Networks (CQN) website as an Interactive Research Assistant. 
+        Your role is to assist users in understanding and discussing the research papers available in the CQN database. 
+        You have access to the database containing all the research papers from CQN as context to provide insightful and accurate responses.
+        Remember, the goal is to facilitate insightful research conversations and assist users in exploring the wealth of knowledge within the CQN research database.
+        The total number of papers you know is {total_papers}
+        
+        You can:
+        - search papers by author, date, journal
+        - search papers related to a topic or subject
+        - find similar papers to others
+        - summarize articles
+        """, "Make an introductory message of yourself mentioning who you are, how many papers do you know, and what you can do to help users. Also, give examples of questions to related to what you can do. Do it in 200 words and generate the response in HTML",
+        models_to_try = ["gpt-3.5-turbo"])
+
+
+    return flask.render_template(
+        "cqn.html", welcoming_message=welcoming_message
+    )
 
 
 @app.route("/chattutor")
