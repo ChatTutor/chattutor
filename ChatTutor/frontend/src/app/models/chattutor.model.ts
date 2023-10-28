2adfc406f9d91c221c7af089098b67a1d0f7d669
Alexandru Ariton <anduariton1234@gmail.com>
2023-10-28 12:24:22 +0300

    Not working

--- ChatTutor/frontend/src/app/models/chattutor.model.ts
+++ ChatTutor/frontend/src/app/models/chattutor.model.ts
@@ -4,6 +4,6 @@ export interface ChatTutor {
     conversation: Message[],
     selectedModel:  "gpt-3.5-turbo-16k" | "gpt-3.5-turbo-8k",
     multiple: boolean,
-    collection: string[] | ['test_embedding']
+    collection?: string[] | ['test_embedding']
     from_doc?: string
 }
