cd455a5f6cc5e4da42436332af8c3c4cb0c89087
Alexandru Ariton <anduariton1234@gmail.com>
2023-10-28 12:24:22 +0300

    Not working

--- ChatTutor/frontend/src/app/app-routing.module.ts
+++ ChatTutor/frontend/src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {CQNChatTutorWrapperComponent} from "./cqnchat-tutor-wrapper/cqnchat-tutor-wrapper.component";
+import {ChatTutorWrapperComponent} from "./chat-tutor-wrapper/chat-tutor-wrapper.component";
 
-const routes: Routes = [];
+const routes: Routes = [{path: 'cqnchattutor', component: CQNChatTutorWrapperComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
