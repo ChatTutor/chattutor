d34228055dea339e80d869dd857da0fc65da5641
Adrian Ariton <adrianariton@Adrian-Minune.local>
2023-10-28 16:45:09 +0300

    added landing

--- ChatTutor/frontend/src/app/app-routing.module.ts
+++ ChatTutor/frontend/src/app/app-routing.module.ts
@@ -2,8 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {CQNChatTutorWrapperComponent} from "./cqnchat-tutor-wrapper/cqnchat-tutor-wrapper.component";
 import {ChatTutorWrapperComponent} from "./chat-tutor-wrapper/chat-tutor-wrapper.component";
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
-const routes: Routes = [{path: 'cqnchattutor', component: CQNChatTutorWrapperComponent}];
+const routes: Routes = [
+  {path: '', component: LandingPageComponent},
+  {path: 'cqnchattutor', component: CQNChatTutorWrapperComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
