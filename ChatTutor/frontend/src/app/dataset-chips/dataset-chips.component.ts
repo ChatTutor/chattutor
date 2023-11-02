cf34ea428907899d347373b5f751a56818d369c4
Adrian Ariton <adrianariton@Adrian-Minune.local>
2023-11-02 15:05:17 +0200

    ui

--- ChatTutor/frontend/src/app/dataset-chips/dataset-chips.component.ts
+++ ChatTutor/frontend/src/app/dataset-chips/dataset-chips.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Component, EventEmitter, Input, OnChanges, Output, SimpleChanges} from '@angular/core';
 import {MatChipInputEvent} from "@angular/material/chips";
 import {COMMA, ENTER, SPACE} from "@angular/cdk/keycodes";
 import { WStatus } from 'app/models/windowstatus.enum';
@@ -8,14 +8,25 @@ import { WStatus } from 'app/models/windowstatus.enum';
     templateUrl: './dataset-chips.component.html',
     styleUrls: ['./dataset-chips.component.css']
 })
-export class DatasetChipsComponent {
+export class DatasetChipsComponent implements OnChanges {
     @Input() files_data: Array<File> = []
     @Input() urls_data: Array<string> = []
     @Input() status: WStatus = WStatus.Idle
-
+    isactive: boolean = false
     @Output() added_urls_event = new EventEmitter<string>()
 
     protected readonly JSON = JSON;
+
+    ngOnChanges(changes: SimpleChanges): void {
+        if (changes['status']) {
+            if (changes['status'].currentValue == WStatus.DragOver)
+                this.isactive = true
+        }
+    }
+
+    toggle() {
+        this.isactive = !this.isactive
+    }
 
     add(event: MatChipInputEvent): void {
         const value = (event.value || '').trim();
