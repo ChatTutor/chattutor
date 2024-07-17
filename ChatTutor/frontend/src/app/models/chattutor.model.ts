35a54c2c607ff5f2108a88f1bcfa7b2d81ab9a4d
Alexandru Ariton <andubandu@icloud.com>
2024-07-17 12:03:59 -0400

    Changed to 4o

--- ChatTutor/frontend/src/app/models/chattutor.model.ts
+++ ChatTutor/frontend/src/app/models/chattutor.model.ts
@@ -2,7 +2,7 @@ import {Message} from "./message.model";
 
 export interface ChatTutor {
     conversation: Message[],
-    selectedModel:  "gpt-3.5-turbo-16k" | "gpt-3.5-turbo-8k",
+    selectedModel:  "gpt-3.5-turbo-16k" | "gpt-3.5-turbo-8k" | "gpt-4o",
     multiple: boolean,
     collection?: string[] | ['test_embedding']
     from_doc?: string
